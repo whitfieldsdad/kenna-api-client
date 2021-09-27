b24bd19ce4c215267beded03bb2dec06895d0907
Tyler Fisher <tylerfisher@tylerfisher.ca>
2021-09-27 15:08:19 -0400

    Release version 0.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='kenna',
-    version='0.0.4',
+    version='0.1.0',
     author='Tyler Fisher',
     author_email='tylerfisher@tylerfisher.ca',
     description="An API client for Kenna Security",
